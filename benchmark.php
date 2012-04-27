52580cffacff92612cc4e26d48b1d9ae69598261
Martin Rusev <martinrusev@zoho.com>
2012-04-27 15:43:32 +0300

    Benchmark updates

--- benchmark.php
+++ benchmark.php
@@ -1,31 +1,39 @@
 <?php
-// Set the exception handler
-require dirname(__FILE__)."/amon.php";
-Amon::config(array('address'=> 'http://127.0.0.1:2464',
-    'protocol' => 'http'));
 
-$time_start = microtime(true);
+$zeromq_benchmark = True;
+$http_benchmark = True;
 
-for ($i = 0; $i < 10000; $i++) {
-    Amon::log("test");
+if($http_benchmark == True) {
+    // Set the exception handler
+    require dirname(__FILE__)."/amon.php";
+    Amon::config(array('address'=> 'http://127.0.0.1:2464',
+        'protocol' => 'http'));
+
+    $time_start = microtime(true);
+
+    for ($i = 0; $i < 10000; $i++) {
+        Amon::log("test");
+    }
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
+
+    echo "HTTP logging: $time seconds\n";
 }
-$time_end = microtime(true);
-$time = $time_end - $time_start;
 
-echo "HTTP logging: $time seconds\n";
+if($zeromq_benchmark == True) {
+    Amon::config(array('address'=> '127.0.0.1:5464',
+        'protocol' => 'zeromq'));
 
+    $time_start = microtime(true);
 
-Amon::config(array('address'=> '127.0.0.1:5464',
-    'protocol' => 'zeromq'));
+    for ($i = 0; $i < 10000; $i++) {
+        Amon::log("test");
+    }
+    $time_end = microtime(true);
+    $time = $time_end - $time_start;
 
-$time_start = microtime(true);
+    echo "ZeroMQ logging: $time seconds\n";
 
-for ($i = 0; $i < 10000; $i++) {
-    Amon::log("test");
 }
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-
-echo "ZeroMQ logging: $time seconds\n";
 
 
